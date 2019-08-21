2f87391f7228707ea1bda527d5563c55d7aedf89
oGsLP <1145234011@qq.com>
2019-08-21 17:21:35 +0800

    :art: remove useless notes and add guiding ones; sort variables

--- crawl_weibo.py
+++ crawl_weibo.py
@@ -1,58 +1,46 @@
 # from selenium import webdriver
 
-import ssl
-import re
+# External import
 
+import re
 import time
 import requests
+import json
+import os
+
+# Internal import
 
 import util.current as current
 from util.change_scale import encode_b64
 from weibo.read_preset import read_preset
 
-import json
-import os
-
-request_params = {"ajwvr": "6", "domain": "100505", "domain_op": "100505", "feed_type": "0", "is_all": "1",
-                  "is_tag": "0", "is_search": "0"}
-# profile_request_params = {"profile_ftype":"1","is_all":"1"}
-
+# Fixed global variables ------------------
 
 y_n_regex = '([yY](es)?)|([Nn](o|ot)?)'
 y_regex = '[yY](es)?'
 n_regex = '[Nn](o|ot)?'
 
 weibo_url = "https://m.weibo.cn/"
-preset_default_path = "./weibo_uid.txt"
+
+# -------------------------------------------
+
+
+# Customizable global variables -------------
 
 pre_id = '230413'
 post_id = '230283'
 
+'''
+WEIBO_SECOND_PROFILE_WEIBO 全部
+WEIBO_SECOND_PROFILE_WEIBO_ORI 原创
+WEIBO_SECOND_PROFILE_WEIBO_VIDEO 视频
+WEIBO_SECOND_PROFILE_WEIBO_ARTICAL 文章
+WEIBO_SECOND_PROFILE_WEIBO_WEIBO_SECOND_PROFILE_WEIBO_PIC 文章
+'''
 weibo_type = 'WEIBO_SECOND_PROFILE_WEIBO_ORI'
 
-# WEIBO_SECOND_PROFILE_WEIBO 全部
-# WEIBO_SECOND_PROFILE_WEIBO_ORI 原创
-# WEIBO_SECOND_PROFILE_WEIBO_VIDEO 视频
-# WEIBO_SECOND_PROFILE_WEIBO_ARTICAL 文章
-# WEIBO_SECOND_PROFILE_WEIBO_WEIBO_SECOND_PROFILE_WEIBO_PIC 文章
-
-cookie_save_file = "cookie.txt"  # 存cookie的文件名
-cookie_update_time_file = "cookie_timestamp.txt"  # 存cookie时间戳的文件名
-image_result_file = "image_result.md"  # 存图片结果的文件名
-
-# 微博 id : https://weibo.com/u/{1900698023},u 后面的值；
-# 有些明显是用的自定义域名，这个 ID 需要你自己去找了
-
-
-# cookie 去网页版获取，具体可以百度
-cookie = 'MLOGIN=0; _T_WM=ec3cbb7caac2b6d765aa1c64e065ee7c; OUTFOX_SEARCH_USER_ID_NCOO=200622491.85643607; WEIBOCN_FROM=1110006030; M_WEIBOCN_PARAMS=luicode%3D10000011%26lfid%3D2302832101822767%26from%3Dpage_100306%26fid%3D2304132101822767_-_WEIBO_SECOND_PROFILE_PIC%26uicode%3D10000011'
-
-# page_total = 1
-cur_page = 1
-n = 1
-page_pics_num = 0
-total_pics_num = 0
-none_sign = False
+request_params = {"ajwvr": "6", "domain": "100505", "domain_op": "100505", "feed_type": "0", "is_all": "1",
+                  "is_tag": "0", "is_search": "0"}
 
 headers = {
     'Accept': 'application/json, text/plain, */*',
@@ -64,19 +52,33 @@ headers = {
     'Host': 'm.weibo.cn',
     'Pragma': 'no-cache',
     'Referer': "url",
+    # User-Agent 根据自己的电脑修改
     'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
     'X-Requested-With': 'XMLHttpRequest'
 }
 
+# cookie 去网页版获取，具体可以百度
+cookie = 'MLOGIN=0; _T_WM=ec3cbb7caac2b6d765aa1c64e065ee7c; OUTFOX_SEARCH_USER_ID_NCOO=200622491.85643607; WEIBOCN_FROM=1110006030; M_WEIBOCN_PARAMS=luicode%3D10000011%26lfid%3D2302832101822767%26from%3Dpage_100306%26fid%3D2304132101822767_-_WEIBO_SECOND_PROFILE_PIC%26uicode%3D10000011'
+
+preset_default_path = "./weibo_uid.txt"
+root = "_pics"
+
+# -------------------------------------------
+
+
+# Simple global variables -------------------
+
+cur_page = 1
+n = 1
+page_pics_num = 0
+total_pics_num = 0
+none_sign = False
 page_total = 0
 uid = ""
 name = ""
-root = ""
 
+# -------------------------------------------
 
-# This restores the same behavior as before.
-# context = ssl._create_unverified_context()
-# urllib.urlopen("https://no-valid-cert", context=context)
 
 def generate_url(_uid):
     return 'https://m.weibo.cn/api/container/getIndex?containerid=' + pre_id + _uid + '_-_' \
@@ -198,19 +200,19 @@ def set_root_path():
         root = val
 
 
-def main():
+def crawl():
     use_preset = input('1)use preset or not? y|n: ').strip()
     while not re.match(y_n_regex, use_preset):
         print("  wrong input, try again! ")
         use_preset = input('2)use preset or not? y|n: ').strip()
 
     if re.match(y_regex, use_preset):
-        preset_path = input('input preset path (default "%s"): ' % preset_default_path).strip()
+        preset_path = input('  input preset path (default "%s"): ' % preset_default_path).strip()
         if len(preset_path) == 0 or os.path.exists(preset_path):
-            print("  preset path exists")
+            print("    preset path exists")
         else:
             preset_path = preset_default_path
-            print('  preset path does not exist, will use default path: "%s"' %
+            print('    preset path does not exist, will use default path: "%s"' %
                   preset_path)
         users = read_preset(preset_path)
         set_root_path()
@@ -224,4 +226,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    crawl()
